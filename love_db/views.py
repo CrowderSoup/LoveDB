61869825cae6464f141b316d814732d0ff4aeb50
Aaron Crowder <aaron.crowder@gmail.com>
2016-02-05 00:43:59 -0700

    Update response

--- love_db/views.py
+++ love_db/views.py
@@ -4,12 +4,15 @@ import requests, os
 from LoveDb.settings import SECRET_KEY, IFTTT_KEY
 
 def index(request):
-    response = '<h1>LoveDb</h1>'
+    response = """<h1>LoveDb</h1>
+    <p>This application was built out of a simple desire to make sure my wife always remembers how much
+    I love her. The source can be found on <a href="https://github.com/CrowderSoup/LoveDB">Github</a>.</p>
+    <br/><br/>
+    <a href="http://crowdersoup.com">Made with Love, by CrowderSoup</a>
+    """
+
     their_key = request.GET.get('key', '')
     my_key = SECRET_KEY
-
-    response += '<ul><li>My Key: ' + my_key + '</li>'
-    response += '<li>Their Key: ' + their_key + '</li></ul>'
 
     if(their_key == my_key):
         reasons = Reason.objects.filter(been_used=False).order_by('-created_date')[:1]
@@ -27,5 +30,4 @@ def index(request):
             reason.been_used = True
             reason.save()
 
-    response += '<a href="http://crowdersoup.com">Made with Love, by CrowderSoup</a>'
     return HttpResponse(response)
