b638e40c2d45b1b446d65d000879b2ccecce74d6
Aaron Crowder <aaron.crowder@gmail.com>
2016-02-05 16:42:25 -0700

    Add filtering to admin

--- love_db/admin.py
+++ love_db/admin.py
@@ -2,4 +2,8 @@ from django.contrib import admin
 
 from .models import Reason
 
-admin.site.register(Reason)
+class ReasonAdmin(admin.ModelAdmin):
+    list_display = ('reason_text', 'created_date', 'been_used')
+    list_filter = ('been_used', 'created_date')
+
+admin.site.register(Reason, ReasonAdmin)
