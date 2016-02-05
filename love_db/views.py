979e88edcb0338fdb4837cea0ca752826fa9e9cf
Aaron Crowder <aaron.crowder@gmail.com>
2016-02-05 00:23:54 -0700

    Update logic for sending note

--- love_db/views.py
+++ love_db/views.py
@@ -8,9 +8,20 @@ def index(request):
     my_key = SECRET_KEY
 
     if(their_key == my_key):
-        reason = Reason.objects.filter(been_used=False).order_by('-created_date')[:1]
-        love_note = {}
-        love_note['value1'] = reason[0].reason_text
-        requests.post("https://maker.ifttt.com/trigger/send_love_note/with/key/{0}".format(IFTTT_KEY), data=love_note)
+        reasons = Reason.objects.filter(been_used=False).order_by('-created_date')[:1]
+
+        if(len(reasons) > 0):
+            reason = reasons[0]
+            
+            love_note = {}
+            love_note['value1'] = reason.reason_text
+
+            # Send via If This Then That
+            requests.post("https://maker.ifttt.com/trigger/send_love_note/with/key/{0}".format(IFTTT_KEY), data=love_note)
+
+            # Update reason so that we don't use the same one twice (wouldn't that be embarrassing!)
+            reason.been_used = True
+            reason.save()
+
 
     return HttpResponse('<a href="http://crowdersoup.com">Made with Love, by CrowderSoup</a>')
